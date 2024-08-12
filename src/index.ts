c30653e2a4230da4fb18a38152a4a34af534ec80
Kanokpit Yowaratch <kanokpit_yow_fth@tdem.toyota-asia.com>
2024-08-12 22:26:10 +0700

    Create limiter middleware

--- src/index.ts
+++ src/index.ts
@@ -1,16 +1,7 @@
 import express, { Request, Response } from 'express';
-import { rateLimit } from 'express-rate-limit';
+import limiter from './middlewares/ratelimit';
 
 const app = express();
-
-const limiter = rateLimit({
-    // windowMs: 15 * 60 * 1000, // 15 = 15 minutes, 1 = 1 minute
-    // windowMs: 1 * 60 * 1000, // 15 = 15 minutes, 1 = 1 minute
-    windowMs: 1 * 5 * 1000, // 15 = 15 minutes, 1 = 1 minute => 60 => 5 = 5 seconds
-    limit: 3, // Limit each IP to 100 requests per `window` (here, per 15 minutes).
-    standardHeaders: 'draft-7', // draft-6: `RateLimit-*` headers; draft-7: combined `RateLimit` header
-    legacyHeaders: false, // Disable the `X-RateLimit-*` headers.
-});
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
---
+++ src/middlewares/ratelimit.ts
@@ -0,0 +1,12 @@
+import { rateLimit } from 'express-rate-limit';
+
+const limiter = rateLimit({
+    // windowMs: 15 * 60 * 1000, // 15 = 15 minutes, 1 = 1 minute
+    // windowMs: 1 * 60 * 1000, // 15 = 15 minutes, 1 = 1 minute
+    windowMs: 1 * 5 * 1000, // 15 = 15 minutes, 1 = 1 minute => 60 => 5 = 5 seconds
+    limit: 3, // Limit each IP to 100 requests per `window` (here, per 15 minutes).
+    standardHeaders: 'draft-7', // draft-6: `RateLimit-*` headers; draft-7: combined `RateLimit` header
+    legacyHeaders: false, // Disable the `X-RateLimit-*` headers.
+});
+
+export default limiter;
